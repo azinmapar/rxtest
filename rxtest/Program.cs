a8e95ff5d153e0a89b4e381f1a91d9990d823adb
azinmapar <azin.mapar@yahoo.com>
2024-08-06 15:09:56 +0330

    Quiescent

--- rxtest/Program.cs
+++ rxtest/Program.cs
@@ -2,11 +2,12 @@
 
 using System.Reactive.Linq;
 
-
+// a simple ticker using rx
 IObservable<long> ticks = Observable.Timer(
     dueTime: TimeSpan.Zero,
     period: TimeSpan.FromSeconds(1));
 
+// results will be shown only after we subscribe
 ticks.Subscribe(
     ticks => Console.WriteLine($"ticks: {ticks}"));
 
---
+++ rxtest/Rx/RxExt.cs
@@ -0,0 +1,29 @@
+﻿using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace rxtest.Rx
+{
+    static class RxExt
+    {
+
+        //  once we return to a state of inactivity, we will want to process everything that just happened,
+        //  so this operator will produce a list containing every value that the source reported in its most recent flurry of activity.
+        public static IObservable<IList<T>> Quiescent<T>(
+            this IObservable<T> src,
+            TimeSpan minimumInactivityPeriod,
+            IScheduler scheduler)
+        {
+            IObservable<int> onoffs =
+                from _ in src
+                from delta in
+                   Observable.Return(1, scheduler)
+                             .Concat(Observable.Return(-1, scheduler)
+                                               .Delay(minimumInactivityPeriod, scheduler))
+                select delta;
+            IObservable<int> outstanding = onoffs.Scan(0, (total, delta) => total + delta);
+            IObservable<int> zeroCrossings = outstanding.Where(total => total == 0);
+            return src.Buffer(zeroCrossings);
+        }
+    }
+
+}
