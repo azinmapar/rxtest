1308d51ccef19eaa49751fbc8b529571b17fc881
azinmapar <azin.mapar@yahoo.com>
2024-08-10 10:36:53 +0330

    ticker extracted

--- rxtest/Program.cs
+++ rxtest/Program.cs
@@ -1,40 +1,10 @@
 
 
+using rxtest.Rx;
 using System.Reactive.Linq;
 
-// a simple ticker using rx
-IObservable<long> ticks = Observable.Timer(
-    dueTime: TimeSpan.Zero,
-    period: TimeSpan.FromSeconds(1));
+RxExamples.RxTicker();
 
-// results will be shown only after we subscribe
-ticks.Subscribe(
-    ticks => Console.WriteLine($"ticks: {ticks}"));
-
+// our app doesn't stop until we press enter
 Console.ReadLine();
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ rxtest/Rx/RxExamples.cs
@@ -0,0 +1,21 @@
+﻿using System.Reactive.Linq;
+
+namespace rxtest.Rx
+{
+    public class RxExamples
+    {
+
+        public static void RxTicker()
+        {
+            // a simple ticker using rx
+            IObservable<long> ticks = Observable.Timer(
+                dueTime: TimeSpan.Zero,
+                period: TimeSpan.FromSeconds(1));
+
+            // results will be shown only after we subscribe
+            ticks.Subscribe(
+                ticks => Console.WriteLine($"ticks: {ticks}"));
+        }
+
+    }
+}
